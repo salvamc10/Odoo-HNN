90fb59aeb0535baf37bd8c933682a36f00005ba5
Salva M <salvador.monreal@bhior.eu>
2025-04-07 12:01:38 +0200

    feat(purchase): permitir edición de fecha de confirmación en OC

---
+++ .github/custom_addons/purchase_edit_date/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Editable Confirmation Date on Purchase Order',
+    'version': '18.0.1.0.0',
+    'summary': 'Permite editar la fecha de confirmación en pedidos de compra',
+    'category': 'Purchases',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_order_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
