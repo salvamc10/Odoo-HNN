fa6ffc4efd26a690501abd3536f5ca0f454e3405
Salva M <salvador.monreal@bhior.eu>
2025-04-07 11:08:14 +0200

    feat(purchase): permitir edición de fecha de confirmación

---
+++ custom_addons/purchase_edit_date/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Editable Confirmation Date on Purchase Order',
+    'version': '18.0.1.0.0',
+    'summary': 'Permite editar la fecha de confirmación en pedidos de compra',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_order_view.xml',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+}
---
+++ custom_addons/purchase_edit_date/models/purchase_order.py
@@ -0,0 +1,5 @@
+from odoo import models
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+    # No se sobrescribe el campo, solo se deja preparado por si se quiere extender en el futuro
