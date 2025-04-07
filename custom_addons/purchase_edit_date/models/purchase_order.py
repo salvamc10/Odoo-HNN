27e4dbb85032ec01abf7cccd51c0bbf1dfebe333
Salva M <salvador.monreal@bhior.eu>
2025-04-07 11:19:48 +0200

    style(purchase_edit_date): correcciones finales de flake8

--- custom_addons/purchase_edit_date/models/purchase_order.py
+++ custom_addons/purchase_edit_date/models/purchase_order.py
@@ -3,4 +3,4 @@ from odoo import models
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
-    # No se sobrescribe el campo, solo se deja preparado por si se quiere extender en el futuro
+    # No se sobrescribe el campo original. Esta clase está preparada para extensiones futuras.
