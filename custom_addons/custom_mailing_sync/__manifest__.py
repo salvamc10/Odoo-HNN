5f7858186ec005a3ec7cfdbc74d60bfabcfd7f5a
Salva M <salvador.monreal@bhior.eu>
2025-04-14 11:04:04 +0200

    feat: agregar módulo de sincronización de Email con Mailing List

---
+++ custom_addons/custom_mailing_sync/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "Sincronización de Email con Mailing List",
+    'version': '18.0.1.0.0',
+    'summary': "Automatiza la incorporación y actualización de contactos en la lista de mailing a partir de los registros en res.partner.",
+    'description': """
+        Este módulo añade la funcionalidad para que:
+            - Al crear un contacto (res.partner) que incluya un correo, se agregue automáticamente un registro en la lista de mailing "BBDD BHIOR BASE DE DATOS ESPAÑA".
+            - Al editar el campo email en el contacto, se actualice el correo en el registro asociado de mailing.contact.
+            
+        Se previene duplicidad mediante la asignación de un campo relacional.
+    """,
+    'author': "Tu Nombre",
+    'license': 'LGPL-3',
+    'depends': [
+        'mailing',
+        'contacts', 
+    ],
+    'data': [
+        
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ custom_addons/custom_mailing_sync/models/mailing_sync.py
@@ -0,0 +1,54 @@
+from odoo import models, fields, api
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    
+    # Campo para relacionar el contacto de mailing creado
+    mailing_contact_id = fields.Many2one('mailing.contact', string="Contacto en Mailing")
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """
+        Al crear un partner, si se incluye un email, se busca la lista de mailing 
+        "BBDD BHIOR BASE DE DATOS ESPAÑA" y se crea un registro en mailing.contact, 
+        asignándolo al partner.
+        """
+        partners = super(ResPartner, self).create(vals_list)
+        mailing_list = self.env['mailing.list'].search([('name', '=', 'BBDD BHIOR BASE DE DATOS ESPAÑA')], limit=1)
+        if mailing_list:
+            for partner in partners:
+                if partner.email:
+                    # Sólo se crea si aún no existe un contacto en mailing
+                    if not partner.mailing_contact_id:
+                        mailing_contact = self.env['mailing.contact'].create({
+                            'email': partner.email,
+                            'name': partner.name or '',
+                            'list_ids': [(4, mailing_list.id)]
+                        })
+                        partner.mailing_contact_id = mailing_contact.id
+        return partners
+
+    def write(self, vals):
+        """
+        Al editar un partner, se comprueba si el campo 'email' está en los valores (vals).
+        Si se edita el email:
+            - Si ya existe un mailing.contact relacionado, se actualiza el email.
+            - Si no existe y se añade el email, se crea el registro en la lista de mailing.
+        """
+        res = super(ResPartner, self).write(vals)
+        mailing_list = self.env['mailing.list'].search([('name', '=', 'BBDD BHIOR BASE DE DATOS ESPAÑA')], limit=1)
+        for partner in self:
+            if 'email' in vals:
+                if partner.mailing_contact_id:
+                    # Actualizamos el email en el mailing.contact relacionado
+                    partner.mailing_contact_id.write({'email': partner.email})
+                else:
+                    # Si el partner no tenía mailing.contact y ahora tiene email, se crea el registro
+                    if partner.email and mailing_list:
+                        mailing_contact = self.env['mailing.contact'].create({
+                            'email': partner.email,
+                            'name': partner.name or '',
+                            'list_ids': [(4, mailing_list.id)]
+                        })
+                        partner.mailing_contact_id = mailing_contact.id
+        return res
