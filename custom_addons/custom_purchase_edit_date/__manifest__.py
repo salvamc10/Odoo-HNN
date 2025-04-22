70de4e23ac5e4effe3039b55403f070ac94533ec
Salva <salvador.monreal@bhior.eu>
2025-04-22 21:59:51 +0200

    Initial commit

---
+++ custom_addons/custom_purchase_edit_date/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Fecha de confirmación editable',
+    'version': '1.0',
+    'summary': 'Permite editar la fecha de confirmación en pedidos de compra',
+    'description': """
+    Este módulo permite editar la fecha de confirmación en los pedidos de compra.
+    Se añade un campo editable en la vista de formulario del pedido de compra para modificar la fecha de confirmación.
+    """,
+    'category': 'Purchases',
+    'license': 'LGPL-3',
+    'author': 'Salva M',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_order_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
