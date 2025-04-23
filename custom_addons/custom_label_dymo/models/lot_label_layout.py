dafe4c08069c1cff9a88523f2cab8cf3d04b5606
Salva <salvador.monreal@bhior.eu>
2025-04-23 19:31:08 +0200

    fix: ondelete corregido

--- custom_addons/custom_label_dymo/models/lot_label_layout.py
+++ custom_addons/custom_label_dymo/models/lot_label_layout.py
@@ -4,7 +4,10 @@ from odoo import models, fields # type: ignore
 class LotLabelLayout(models.TransientModel):
     _inherit = "lot.label.layout"
 
-    print_format = fields.Selection(selection_add=[('dymo', 'Dymo')], ondelete="set default")
+    print_format = fields.Selection(
+        selection_add=[('dymo', 'Dymo')],
+        ondelete={'dymo': 'set default'}
+    )
 
     def _get_report_name(self):
         if self.print_format == 'dymo':
