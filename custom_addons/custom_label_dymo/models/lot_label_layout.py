2f939093169992985a1bb99e309862b3592fbdb7
Salva <salvador.monreal@bhior.eu>
2025-04-23 19:23:50 +0200

    fix: corrección al añadir una nueva opción y no sobreescribir

--- custom_addons/custom_label_dymo/models/lot_label_layout.py
+++ custom_addons/custom_label_dymo/models/lot_label_layout.py
@@ -4,12 +4,9 @@ from odoo import models, fields # type: ignore
 class LotLabelLayout(models.TransientModel):
     _inherit = "lot.label.layout"
 
-    print_format = fields.Selection([
-        ('4x12', '4 x 12'),
-        ('zpl', 'ZPL Labels'),
-        ('dymo', 'Dymo')], string="Format", default='4x12', required=True)
+    print_format = fields.Selection(selection_add=[('dymo', 'Dymo')])
 
     def _get_report_name(self):
         if self.print_format == 'dymo':
-            return 'product.report_producttemplatelabel_dymo'
+            return 'custom_label_dymo.report_lotlabel_dymo'
         return super()._get_report_name()
