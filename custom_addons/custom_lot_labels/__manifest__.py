43a77a82d3d65a10bd4aca157169d0a40a724383
Salva M <salvador.monreal@bhior.eu>
2025-04-28 08:39:50 +0200

    feat: adaptación etiquetas Dymo y A4 para formatos por producto y por número de serie/Lote

---
+++ custom_addons/custom_lot_labels/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "Etiquetas Personalizadas para números de serie/lote",
+    'version': '18.0.1.0',
+    'summary': "Impresión de etiquetas Dymo y A4 para números de serie o lote",
+    "license": "AGPL-3",
+    "author": "Salva M",
+    "category": "Inventory",
+    "depends": ["stock", "hr_payroll"],
+    "description": ''' 
+    Impresión de etiquetas Dymo y A4 para números de serie o lote.
+    Este módulo permite imprimir etiquetas personalizadas para números de serie o lote en formato Dymo y A4.
+    Las etiquetas pueden incluir información como el nombre del producto, el número de serie o lote, y otros detalles relevantes.
+    Para utilizar las dimensiones correctas de las etiquetas Dymo en este módulo, es necesario modificarlo manualmente:
+        - Ancho de página: **25 mm**
+        - Alto de página: **45 mm**
+        - Márgenes: **0 mm** en todos los lados
+        - Orientación: **Landscape**
+    ''',
+    'data': [
+        'report/lot_label_A4_report.xml',
+        'report/label_dymo_override.xml',
+    ],
+    "installable": True,
+    'application': False,
+}
