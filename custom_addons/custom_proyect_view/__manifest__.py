47c9db0005fd178fdf926f7f749b6fa172003c28
Salva M <salvador.monreal@bhior.eu>
2025-05-26 11:25:40 +0200

    feature: vista para el módulo de proyectos personalizada

---
+++ custom_addons/custom_proyect_view/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Proyectos por departamento',
+    'author': 'Salva M',
+    'version': '18.0.1.0',
+    'category': 'Project',
+    'summary': 'Pantalla inicial por Departamento > Proyecto > Tareas',
+    'depends': ['project', 'hr'],
+    'data': [
+        'views/hr_department_kanban.xml',
+        'views/project_filtered_by_department.xml',
+        'views/task_filtered_by_project.xml',
+        'views/menu_items.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
