05da084d7cc29e2da67727a9af9be5fd2989f262
mayorgonzalez <pedromayorgonzalez44@gmail.com>
2025-06-05 16:00:33 +0200

    fix: elimino codigo dentro de __init__.py de modulo custom_lot_label

--- custom_addons/custom_lot_labels/__init__.py
+++ custom_addons/custom_lot_labels/__init__.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import models
 
-def pre_init_hook(cr):
-    """ Este hook se ejecuta antes de la instalación del módulo """
-    from odoo import api, SUPERUSER_ID # type: ignore
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    env['stock.lot']._create_missing_field()
+
