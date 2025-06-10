11d5989154b38329ee0a7299700dcf1ef0fb6f67
mayorgonzalez <pedromayorgonzalez44@gmail.com>
2025-06-10 08:42:39 +0200

    fix : nuevos modelos en modulo custom_stock_lot_view

--- custom_addons/custom_projects_view/models/project.py
+++ custom_addons/custom_projects_view/models/project.py
@@ -6,9 +6,6 @@ class ProjectProject(models.Model):
     department_id = fields.Many2one(
         'hr.department',
         string="Departamento",
-<<<<<<< HEAD
         required=True,
-=======
->>>>>>> 7b39f473d48bb166860e6c406b057bf11508deb8
         help="Departamento responsable del proyecto."
     )
