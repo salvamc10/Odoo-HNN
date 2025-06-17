a6c94afc2953c85c5ccb32567fc1ac10cd569c98
mayorgonzalez <pedromayorgonzalez44@gmail.com>
2025-06-17 16:42:18 +0200

    feature: custom_purchase_lines_pending_receipt

---
+++ custom_addons/custom_purchase_lines_pending_receipt/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Purchase Lines Pending Receipt",
+    "version": "1.0",
+    "depends": ["purchase"],
+    "category": "Purchases",
+    "author": "Pedro m.",
+    "summary": "Vista de líneas de pedido pendientes de recepción",
+    "description": "Agrega una vista de líneas de pedido donde la cantidad pedida es mayor a la recibida.",
+    "data": [
+        "views/purchase_lines_pending_view.xml"
+    ],
+    "installable": True,
+    "application": False,
+}
