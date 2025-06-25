16fa1d4b9f6b348d7d9e1a14019d69ca418951d6
Salva M <salvador.monreal@bhior.eu>
2025-06-25 12:21:26 +0200

    feat: órdenes de fabricación automáticas tras validar la compra de máquinas

---
+++ custom_addons/custom_mrp_automation/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Custom_mrp_automation',
+    'version': '18.0.1.0.0',
+    'summary': """ Automatiza la creación de órdenes de fabricación tras validar recepciones """,
+    'author': 'Pedro M.',
+    'website': '',
+    'category': 'Manufacturing',
+    'depends': ['base', 'mrp', 'stock'],
+    "data": [        
+    ],    
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
