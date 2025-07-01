6c5434d77c17b315bc043c51cd1109e325285fc0
mayorgonzalez <pedromayorgonzalez44@gmail.com>
2025-07-01 09:39:02 +0200

    feature: nuevo modulo

---
+++ custom_addons/custom_ce_template/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Hoja CE por producto en pedido de venta',
+    'summary': 'Genera una hoja CE individual por producto o lote en cada pedido de venta',
+    'version': '18.0.1.0',
+    'author': 'Pedro M',
+    'license': 'LGPL-3',
+    'category': 'Sales/CRM',
+    'depends': ['sale', 'stock'],
+    'data': [
+        'report/ce_report_template_stock.xml',
+        'report/ce_report_action_stock.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
