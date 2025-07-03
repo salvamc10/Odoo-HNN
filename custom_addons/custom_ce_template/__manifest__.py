5b20de90b58c5e279c6482402487b88dbdaa5b07
mayorgonzalez <pedromayorgonzalez44@gmail.com>
2025-07-03 11:30:18 +0200

    fix: nuevos arreglos cambio de imagenes

--- custom_addons/custom_ce_template/__manifest__.py
+++ custom_addons/custom_ce_template/__manifest__.py
@@ -5,7 +5,7 @@
     'author': 'Pedro M',
     'license': 'LGPL-3',
     'category': 'Sales/CRM',
-    'depends': ['sale', 'stock'],
+    'depends': ['sale', 'stock', 'web'],
     "data": [
         "report/ir_actions_report_templates.xml",
         "report/ir_actions_report.xml",
