6b968ddcb207827a3c3c10fd9575ef4d3cb8a1c5
mayorgonzalez <pedromayorgonzalez44@gmail.com>
2025-07-16 08:20:00 +0200

    feat: importar módulo mi_modulo desde rama prueba

---
+++ custom_addons/custom_ce_template/models/product_template.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    invoice_attachment_id = fields.Many2one(
+        'ir.attachment',
+        string='Documento a adjuntar en factura',
+        domain="[('res_model', '=', 'product.template')]"
+    )
