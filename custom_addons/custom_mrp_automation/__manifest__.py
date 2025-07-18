dc07abb3d0cc744c785cc1280cb9486d8fde334f
Salva <salvador.monreal@bhior.eu>
2025-07-18 20:01:19 +0200

    fix: pruebas combinando versiones

--- custom_addons/custom_mrp_automation/__manifest__.py
+++ custom_addons/custom_mrp_automation/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Custom_mrp_automation',
+    'name': 'Orders de Fabricación Automáticas',
     'version': '18.0.1.0.0',
     'summary': """ Automatiza la creación de órdenes de fabricación tras validar recepciones """,
     'author': 'Pedro M.',
