ae86c1449ef83e7929cf8835cadd6c8883e2f0ab
Salva M <salvador.monreal@bhior.eu>
2025-08-11 10:23:59 +0200

    refactor: cambios de prueba para comprobar el gitflow

--- custom_addons/custom_projects_view/__manifest__.py
+++ custom_addons/custom_projects_view/__manifest__.py
@@ -1,9 +1,10 @@
 {
-    'name': "Vista de proyectos con departamentos",
-    'summary': "Sidebar de departamentos en proyectos como en empleados/contactos",
+    'name': "Vista de proyectos dividida por departamentos",
+    'summary': "Sidebar de departamentos en proyectos como en el módulo de empleados",
     'description': """
-    Este módulo añade una barra lateral de departamentos en la vista kanban de proyectos, similar a la que existe en empleados y contactos. 
+    Este módulo añade una barra lateral de departamentos en la vista kanban de proyectos, similar a la que existe en empleados. 
     Permite filtrar proyectos por departamento, facilitando la gestión y visualización de proyectos relacionados con diferentes áreas de la empresa.
+    Esta funcionalidad es especialmente útil para empresas que manejan múltiples proyectos y desean organizarlos por departamentos para una mejor visibilidad y gestión.
     """,
     'author': "Salva M",
     'category': 'Project',
