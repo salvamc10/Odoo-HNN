61e241ebf6f764b6cf455e0bea1f44c7b01d5116
mayorGonzalez <pedromayorgonzalez44@gmail.com>
2025-08-21 10:50:51 +0200

    fix: nuevas modificaciones en la clase repair_consultas

--- custom_addons/custom_repair_product/models/repair_consultas.py
+++ custom_addons/custom_repair_product/models/repair_consultas.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 class RepairConsulta(models.Model):
     _name = 'repair.consulta'
