cdbb01a6f7b04eb30d909f7ce638d27f355c7ab9
mayorGonzalez <pedromayorgonzalez44@gmail.com>
2025-08-25 15:36:56 +0200

    fix: añado boton en vista consulta

--- custom_addons/custom_repair_product/models/repair_consultas.py
+++ custom_addons/custom_repair_product/models/repair_consultas.py
@@ -32,27 +32,11 @@ class RepairConsulta(models.Model):
             product = self.env['product.product'].search(domain, limit=1)
             # Si se encuentra un producto, asigna su ID; si no, deja product_id en blanco
             self.product_id = product.id if product else False
-            # # Guardar el registro si ya existe
-            # if self._origin:
-            #     self.write({
-            #         'product_id': self.product_id,
-            #         'consulta_text': self.consulta_text,
-            #         'refer': self.refer,
-            #         'product_uom_qty': self.product_uom_qty,
-            #         'picked': self.picked,
-            #     })
+            
         else:
             # Si ambos campos están vacíos, limpia product_id
             self.product_id = False
-            # # Guardar el registro si ya existe
-            # if self._origin:
-            #     self.write({
-            #         'product_id': False,
-            #         'consulta_text': self.consulta_text,
-            #         'refer': self.refer,
-            #         'product_uom_qty': self.product_uom_qty,
-            #         'picked': self.picked,
-            #     })
+            
 
     def action_create_product(self):
         """Devuelve una acción para crear un nuevo producto y actualiza product_id."""
