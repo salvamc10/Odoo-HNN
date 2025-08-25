e2cf9e8974557d0e2bd7497ee9a7da72bbaf927b
mayorGonzalez <pedromayorgonzalez44@gmail.com>
2025-08-25 14:38:13 +0200

    fix: añado boton en vista consulta

--- custom_addons/custom_repair_product/models/repair_order.py
+++ custom_addons/custom_repair_product/models/repair_order.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 class RepairOrder(models.Model):
     _inherit = 'repair.order'
