742e7e86c5e970390be19d9a38fffbbb62f67a9a
Salva M <salvador.monreal@bhior.eu>
2025-08-26 09:04:27 +0200

    feat: módulo para mantener ancho de las columnas

--- custom_addons/custom_repair_product/models/repair_order.py
+++
@@ -1,9 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import fields, models
-
-class RepairOrder(models.Model):
-    _inherit = 'repair.order'
-
-    consulta_text = fields.Char(string='Consulta')
-
