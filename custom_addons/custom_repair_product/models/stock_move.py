fc44e739497f9600dba4947b2d6e8f5af50f74b1
mayorGonzalez <pedromayorgonzalez44@gmail.com>
2025-08-26 13:32:46 +0200

    fix: nuevos ajuste para añadir filtro

--- custom_addons/custom_repair_product/models/stock_move.py
+++ custom_addons/custom_repair_product/models/stock_move.py
@@ -5,11 +5,12 @@ class StockMove(models.Model):
 
     lot_id = fields.Many2one('stock.lot', string="Lot/Serial Number", domain="[('product_id', '=', product_id), ('company_id', '=', company_id)]")
     repair_state = fields.Selection(related='repair_id.state', store=True, string="Estado reparación")
+    partner_repair_id = fields.Many2one(related='repair_id.partner_id', string="Cliente", store=True)
     consulta_text = fields.Char(string='Consulta')
     x_machine_number = fields.Char(related='lot_id.x_machine_number', string="Número de Máquina/Lote", store=True)
     internal_reference = fields.Char(related='lot_id.ref', string="Referencia Interna", store=True)    
     provider_reference = fields.Char(related='provider_id.product_code', string="Referencia Proveedor", store=True)
-    provider_id = fields.Many2one('product.supplierinfo', string="Proveedor", domain="[('supplier_rank', '>', 0)]")
+    provider_id = fields.Many2one('product.supplierinfo', string="Proveedor", store=True)
     description = fields.Html(related='lot_id.note', string="Descripción", store=True)
     estado_recambio = fields.Selection(
             string="Estado Recambio",
