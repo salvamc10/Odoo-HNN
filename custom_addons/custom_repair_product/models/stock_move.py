99368891164c7f92a348957ca3cc8895834f468d
mayorGonzalez <pedromayorgonzalez44@gmail.com>
2025-08-26 09:58:55 +0200

    fix: nuevos ajuste para añadir filtro

--- custom_addons/custom_repair_product/models/stock_move.py
+++ custom_addons/custom_repair_product/models/stock_move.py
@@ -1,10 +1,9 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 from odoo import fields, models
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
+    lot_id = fields.Many2one('stock.lot', string="Lot/Serial Number", domain="[('product_id', '=', product_id), ('company_id', '=', company_id)]")
     repair_state = fields.Selection(related='repair_id.state', store=True, string="Estado reparación")
     consulta_text = fields.Char(string='Consulta')
     x_machine_number = fields.Char(related='lot_id.x_machine_number', string="Número de Máquina/Lote", store=True)
