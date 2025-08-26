c17b6ab30d4a54896a43e6f7119fee2b65fbac66
mayorGonzalez <pedromayorgonzalez44@gmail.com>
2025-08-26 11:38:18 +0200

    fix: nuevos ajuste para añadir filtro

--- custom_addons/custom_repair_product/models/stock_move.py
+++ custom_addons/custom_repair_product/models/stock_move.py
@@ -7,8 +7,7 @@ class StockMove(models.Model):
     repair_state = fields.Selection(related='repair_id.state', store=True, string="Estado reparación")
     consulta_text = fields.Char(string='Consulta')
     x_machine_number = fields.Char(related='lot_id.x_machine_number', string="Número de Máquina/Lote", store=True)
-    internal_reference = fields.Char(related='lot_id.ref', string="Referencia Interna", store=True)
-    description = fields.Text(related='lot_id.note', string="Descripción", store=True)
+    internal_reference = fields.Char(related='lot_id.ref', string="Referencia Interna", store=True)    
     provider_reference = fields.Char(related='provider_id.product_code', string="Referencia Proveedor", store=True)
     provider_id = fields.Many2one('product.supplierinfo', string="Proveedor", domain="[('supplier_rank', '>', 0)]")
     
