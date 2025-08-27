4e36f1ba419b391c67b5e9a27e9bb20e8f20266b
mayorGonzalez <pedromayorgonzalez44@gmail.com>
2025-08-27 14:11:01 +0200

    feat(custom_sale_recurring_invoicing): incorporación del módulo para facturación recurrente en ventas

---
+++ custom_addons/custom_sale_recurring_invoicing/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Custom Sale Recurring Invoicing',
+    'version': '18.0.1.0.0',
+    'summary': 'Gestión de facturación recurrente en ventas.',
+    'author': 'Pedro M.',    
+    'category': 'Sales',
+    'depends': ['base', 'sale'],
+    'data': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
