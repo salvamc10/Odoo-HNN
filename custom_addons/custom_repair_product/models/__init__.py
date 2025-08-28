bbaa340405dcb8608012d49bee37fc1df287aeac
mayorGonzalez <pedromayorgonzalez44@gmail.com>
2025-08-28 13:13:35 +0200

    fix: mergeo cambios

--- custom_addons/custom_repair_product/models/__init__.py
+++ custom_addons/custom_repair_product/models/__init__.py
@@ -1,8 +1,4 @@
 from . import stock_move
-<<<<<<< HEAD
-from . import repair_order
-=======
 from . import repair_order
 from . import repair_consultas
 from . import stock_lot
->>>>>>> fa564c68c58d72b7d90427d683e9d227c505a725
