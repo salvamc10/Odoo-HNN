ebc7bb38d234b036e4112f0d4c5f442f23e12811
mayorgonzalez <pedromayorgonzalez44@gmail.com>
2025-08-29 19:30:40 +0200

    [FIX] subo los cambios

--- custom_addons/custom_funcional/static/components/chatter/chatter.esm.js
+++ custom_addons/custom_funcional/static/components/chatter/chatter.esm.js
@@ -5,7 +5,7 @@ import {Chatter} from "@mail/chatter/web_portal/chatter";
 import {patch} from "@web/core/utils/patch";
 import { useState } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
-import { use } from "react";
+
 
 patch(Chatter.prototype, {
     setup() {
@@ -25,9 +25,44 @@ patch(Chatter.prototype, {
 
     async onToggleFavorite() {
         const favorite = await this.isFavorite();
-        if (favorite.length > 0) {
+        if (favorite.length > 0)  {
             if (favorite.length > 0) {
                 this.state.favorite = false;
                 await this.orm.unlink("my.favorite", favorite[0].id);
+            }
+            this.notification.add(_t("Removed from favorites"), {
+                type: "danger",
+                sticky: false
+            });
 
-})
+        } else {
+            this.state.favorite = true;
+            await this.orm.create("my.favorite", [
+                {
+                    user_id: this.env.model.config.context.uid,
+                    res_model: this.threadModel,
+                    res_id: this.threadId,
+                },
+            ]);
+            this.notification.add(_t("Added to favorites"), {
+                type: "success",
+                sticky: true,                               
+            });
+        }
+        this.ui.bus.trigger("UI-REFRESH-FAVORITE");
+    },
+
+    async isFavorite() {
+        const favorite = await this.orm.searchRead(
+            "my.favorite",
+            [
+                ["user_id", "=", this.env.model.config.context.uid],
+                ["res_model", "=", this.threadModel],
+                ["res_id", "=", this.threadId],
+            ],
+            ["id"]
+        );
+        this.state.favorite = favorite.length > 0;
+        return favorite;
+    },
+});
