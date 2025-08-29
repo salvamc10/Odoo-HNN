d46f4d40ce78ebaea2a34e47affde219b1f3fd8b
mayorgonzalez <pedromayorgonzalez44@gmail.com>
2025-08-29 13:33:16 +0200

    [FIX]: nuevos ajustes en el modulo repair_order

--- custom_addons/custom_repair_product/models/__init__.py
+++ custom_addons/custom_repair_product/models/__init__.py
@@ -1,6 +1,6 @@
+from . import repair_worksheet_template
 from . import stock_move
 from . import repair_order
 from . import repair_consultas
 from . import stock_lot
-from . import repair_worksheet_template
 
