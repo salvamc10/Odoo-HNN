97401badc0bbd086ec0ac29bd574f2d0d4f5218a
mayorgonzalez <pedromayorgonzalez44@gmail.com>
2025-08-30 13:02:32 +0200

    [FIX] Nuevos cambios modulo repair_ sign

--- custom_addons/custom_repair_product/models/worksheet_template.py
+++ custom_addons/custom_repair_product/models/worksheet_template.py
@@ -1,9 +1,8 @@
-from odoo import models, fields, api, _
+from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
 class WorksheetTemplate(models.Model):
     _inherit = 'worksheet.template'
-    _description = 'Plantilla de Hoja de Trabajo de Reparación'
 
     @api.depends('name')
     def _compute_model_id(self):
@@ -12,24 +11,43 @@ class WorksheetTemplate(models.Model):
             model_name = 'x_repair_worksheet_%s' % template.id
             template.model_id = IrModel.search([('model', '=', model_name)], limit=1)
             if not template.model_id and template.name:
-                # Crear modelo dinámico si no existe
                 model_vals = {
                     'name': _('Hoja de Trabajo: %s') % template.name,
                     'model': model_name,
-                    'inherited_model_ids': [(6, 0, [self.env.ref('custom_repair_product.model_repair_worksheet').id])],  # Hereda del base (ajusta ref si es XML ID)
+                    'inherited_model_ids': [(6, 0, [self.env.ref('custom_repair_product.model_repair_worksheet').id])],
                     'state': 'manual',
                     'transient': False,
                 }
                 template.model_id = IrModel.sudo().create(model_vals)
-                # Crear vista form básica si no existe
                 if not self.env['ir.ui.view'].search([('model', '=', model_name), ('type', '=', 'form')]):
                     self.env['ir.ui.view'].sudo().create({
                         'name': _('Vista Form por Defecto: %s') % template.name,
                         'model': model_name,
                         'type': 'form',
-                        'arch': '<form string="%s"><sheet><group><field name="notes"/></group></sheet></form>' % template.name,
+                        'arch': '''
+                            <form string="%s">
+                                <sheet>
+                                    <group>
+                                        <field name="notes"/>
+                                        <field name="worksheet_signature" widget="signature"/>
+                                    </group>
+                                </sheet>
+                            </form>
+                        ''' % template.name,
                     })
-                    
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """Sobrescribe create para asegurar la creación del modelo dinámico en lotes"""
+        for vals in vals_list:
+            if not vals.get('res_model'):
+                vals['res_model'] = 'repair.order'
+        templates = super().create(vals_list)
+        for template in templates:
+            if not template.model_id:
+                template._compute_model_id()
+        return templates
+
     def action_edit_template(self):
         self.ensure_one()
         if not self.model_id:
@@ -45,10 +63,3 @@ class WorksheetTemplate(models.Model):
                 'studio': True,
             },
         }
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        templates = super().create(vals_list)
-        for template in templates:
-            template._compute_model_id()  # Asegura creación inmediata
-        return templates
