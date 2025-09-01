4ead8a28be5816940499de8845423f8015af4c3f
mayorGonzalez <pedromayorgonzalez44@gmail.com>
2025-09-01 09:50:04 +0200

    [FIX] soluciono nuevos errores

--- custom_addons/custom_repair_product/models/fsm_report.py
+++ custom_addons/custom_repair_product/models/fsm_report.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api
 
 class FSMReport(models.Model):
-    _inherit = "fsm.report"
+    _inherit = "industry.fsm.report"
 
     repair_id = fields.Many2one("repair.order", string="Reparación")
