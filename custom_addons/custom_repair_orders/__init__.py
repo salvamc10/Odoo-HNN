8ca68dc238d23ffb6241e1d37bad821e0e2eca25
Salva M <salvador.monreal@bhior.eu>
2025-09-11 09:29:28 +0200

    release: limpieza de rama

--- custom_addons/custom_repair_orders/__init__.py
+++ custom_addons/custom_repair_orders/__init__.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 from . import models
 from . import wizards
 
+=======
+from . import models
+>>>>>>> 1132153a1b44b6147031c3661f79b4cc2c49ea79
--- custom_addons/custom_repair_product/__manifest__.py
+++ custom_addons/custom_repair_product/__manifest__.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 {
 
     'name': 'Productos para la reparación',
@@ -36,3 +37,43 @@
     'application': False,
     'auto_install': False,
 }
+=======
+{
+
+    'name': 'Productos para la reparación',
+
+    'version': '18.0.1.0',
+    'author': 'Pedro M',
+    'license': 'LGPL-3',
+    'summary': 'Gestión de productos asociados a órdenes de reparación',
+    'description': """
+    Este módulo permite gestionar de manera eficiente los productos que están asociados a órdenes de reparación.
+    Incluye funcionalidades para rastrear el estado de los productos, gestionar su inventario y facilitar
+    la creación de órdenes de trabajo.
+    """,
+    'category': 'Operations/Repair',
+    'depends': [
+        'repair',
+        'product',
+        'stock',
+        'web_studio',
+        'worksheet',
+        'base',
+        'project',
+        'documents',
+    ],
+    'data': [
+       'security/repair_groups.xml',
+       'security/ir.model.access.csv',  
+       'security/worksheet_template_rule.xml',       
+       'views/repair_product_views.xml',
+       'views/repair_order_views.xml',
+        'views/repair_order_list_custom_view.xml',      
+        'report/worksheet_report.xml',
+       'wizards/repair_worksheet_wizard_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+>>>>>>> 1132153a1b44b6147031c3661f79b4cc2c49ea79
