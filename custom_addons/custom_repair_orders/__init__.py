b4a3fb551cb5c2dc50a8e871fc25988e220c93d2
Salva M <salvador.monreal@bhior.eu>
2025-09-11 10:09:34 +0200

    release: limpieza de rama

--- custom_addons/custom_repair_orders/__init__.py
+++ custom_addons/custom_repair_orders/__init__.py
@@ -1,11 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 from . import models
-from . import wizards
-
-=======
-from . import models
->>>>>>> 1132153a1b44b6147031c3661f79b4cc2c49ea79
-=======
-from . import models
->>>>>>> 1132153a1b44b6147031c3661f79b4cc2c49ea79
