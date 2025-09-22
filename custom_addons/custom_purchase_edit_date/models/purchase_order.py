1a6f8cbd8eba6ce9c0ca28c6b75d2a07fba91f09
Salva M <salvador.monreal@bhior.eu>
2025-09-22 11:01:32 +0200

    release: módulos adicionales eliminados

--- custom_addons/custom_purchase_edit_date/models/purchase_order.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
-
-    date_approve = fields.Datetime(
-        string="Fecha de confirmación",
-        readonly=False
-    )
