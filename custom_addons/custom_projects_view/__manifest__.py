ccc2c3d673a615ea5265825298fa103bcb2dc5bf
Salva <salvador.monreal@bhior.eu>
2025-10-24 21:22:24 +0200

    Initial commit

--- custom_addons/custom_projects_view/__manifest__.py
+++ custom_addons/custom_projects_view/__manifest__.py
@@ -4,6 +4,7 @@
     'description': """
     Este módulo añade una barra lateral de departamentos en la vista kanban de proyectos, similar a la que existe en empleados y contactos. 
     Permite filtrar proyectos por departamento, facilitando la gestión y visualización de proyectos relacionados con diferentes áreas de la empresa.
+    No mostrar en la vista de proyectos los departamentos que no tienen proyectos asignados.
     """,
     'author': "Salva M",
     'category': 'Project',
